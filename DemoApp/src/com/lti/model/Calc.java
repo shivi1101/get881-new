181882ca87c6983743814b82795d976677e73347
shivi1101 <62545678+shivi1101@users.noreply.github.com>
2020-12-18 16:32:54 +0530

    deleted add

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -2,12 +2,11 @@ package com.lti.model;
 
 public class Calc {
 
-	public void add(int n1, int n2) {
-		System.out.println("Sum= "+(n1+n2));
-	}
+	
 	public void difference(int n3, int n4) {
 		System.out.println("Difference= "+(n3-n4));
 	}
+	
 	
 	
 }
