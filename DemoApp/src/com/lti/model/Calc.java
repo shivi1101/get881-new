920e12b319e4082a61609c9ae8f3367174923a3d
Shivi Gupta <Shivi Gupta@LAPTOP-5MUKFOSJ>
2020-12-18 16:41:38 +0530

    added sub

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -5,6 +5,9 @@ public class Calc {
 	public void add(int n1, int n2) {
 		System.out.println("Summation= "+(n1+n2));
 	}
+	public void sub(int n1, int n2) {
+		System.out.println("diff= "+(n1-n2));
+	}
 	
 	
 	
