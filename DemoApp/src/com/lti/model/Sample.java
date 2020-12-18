4a7a748a21e71d11fa102ded031ed750e13e1b9c
Shivi Gupta <Shivi Gupta@LAPTOP-5MUKFOSJ>
2020-12-18 16:06:38 +0530

    first commit

---
+++ DemoApp/src/com/lti/model/Sample.java
@@ -0,0 +1,12 @@
+package com.lti.model;
+
+public class Sample {
+
+	public void show() {
+		System.out.println("hello world");
+	}
+		public void method() {
+			System.out.println("hi!!1 how are u");
+		
+	}
+}
